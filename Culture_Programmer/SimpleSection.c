758790f78ea72e742b2b4329cbce6cefb0248c93
周飞 <1174977961@qq.com>
2019-05-16 22:57:27 +0800

    add: 添加SimpleSection类

---
+++ Culture_Programmer/SimpleSection.c
@@ -0,0 +1,29 @@
+//
+//  SimpleSection.c
+//  C_Pro
+//
+//  Created by 周飞 on 2019/5/16.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#include "SimpleSection.h"
+
+int printf(const char *format,...);
+
+int global_init_var = 84;
+int global_uninit_var;
+
+void func1(int i) {
+    printf("%d\n",i);
+}
+
+int main(void) {
+    static int static_var = 85;
+    static int static_var2;
+    
+    int a = 1;
+    int b;
+    
+    func1(static_var+static_var2+a+b);
+    return a;
+}
