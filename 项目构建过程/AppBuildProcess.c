e73569ced2699431a0e262cabfd94df1c38890f0
周飞 <1174977961@qq.com>
2019-09-03 23:57:23 +0800

    add: 添加编译过程文件

---
+++ 项目构建过程/AppBuildProcess.c
@@ -0,0 +1,15 @@
+//
+//  AppBuildProcess.c
+//  Culture_Programmer
+//
+//  Created by 周飞 on 2019/9/3.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#include "AppBuildProcess.h"
+
+int main(int argc, char *argv[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ 项目构建过程/AppBuildProcess.h
@@ -0,0 +1,25 @@
+//
+//  AppBuildProcess.h
+//  Culture_Programmer
+//
+//  Created by 周飞 on 2019/9/3.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+/*
+ 预编译过程
+ 在编译过程中，预编译后就停止
+ xcrun clang -E helloworld.c
+ //通过管道在编辑器中输出
+ xcrun clang -E helloworld.c | open -f
+ 
+ 在xcode中，可以通过工具生成任意文件的预处理文件
+ Product -> Perform Action -> Preprocess
+ */
+
+#ifndef AppBuildProcess_h
+#define AppBuildProcess_h
+
+#include <stdio.h>
+
+#endif /* AppBuildProcess_h */
